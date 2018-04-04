46e42cf9300600263b69e8bf832cf216f9b11eab
Buglight <buglight@kistriver.com>
2018-04-04 23:04:29 +0300

    Base project

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/main.js',
+    mode: 'development',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+            inject: true
+        })
+    ]
+};
