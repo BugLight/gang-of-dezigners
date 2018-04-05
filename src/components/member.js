10b196b1cf57e97c10180430ce5ac5718d89912b
Gorbunov Pavel <pachagorbunoff@yandex.ru>
2018-04-06 00:06:24 +0300

    #5 one argument to child

--- src/components/member.js
+++ src/components/member.js
@@ -1,16 +1,16 @@
 export default {
-	props : ['nick', 'first', 'last', 'group', 'desc'],
+	props : ['info'],
     computed: {
         fullname () {
-            return this.first + ' ' + this.last;
+            return this.info.first + ' ' + this.info.last;
         }
     },
 	render (h) {
 		return <div class="member">
-    		<h1 class="member__nick">{this.nick}</h1>
+    		<h1 class="member__nick">{this.info.nick}</h1>
     		<h2 class="member__name">{this.fullname}</h2>
-    		<h2 class="member__group">{this.group}</h2>
-    		<p class="member__desc">{this.desc}</p>
+    		<h2 class="member__group">{this.info.group}</h2>
+    		<p class="member__desc">{this.info.desc}</p>
 		</div>;
 	}
 }
