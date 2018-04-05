8cad97ca1911d88e53bb11c326505ca2d07bd798
pettro98 <ya.skywalker@yandex.ru>
2018-04-06 01:00:18 +0300

    added footer component

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,15 @@
 import app_header from './app-header'
+import app_footer from './app-footer'
 
 export default {
     components: {
-        'app-header': app_header
+        'app-header': app_header,
+        'app-footer': app_footer
     },
     render (h) {
         return <div>
             <app-header></app-header>
-        </div>;
+            <app-footer></app-footer>
+          </div>;
     }
 };
