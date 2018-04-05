ed1cde443a0a07a726b68f790cf3d562854c97eb
pettro98 <ya.skywalker@yandex.ru>
2018-04-05 19:59:41 +0300

    added some style to header

--- src/components/app-header.js
+++ src/components/app-header.js
@@ -2,7 +2,6 @@ export default {
     render (h) {
       return <header class="header">
         <span class="header__team-name">Gang of Dezigners</span>
-        <span>% кароч наш слоган/девиз %</span>
       </header>
     }
 };
