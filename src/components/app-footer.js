aa5dfd3dceaba4c63203e3d337c5517e50b01984
Buglight <buglight@kistriver.com>
2018-04-06 23:49:18 +0300

    Footer fixed

--- src/components/app-footer.js
+++ src/components/app-footer.js
@@ -1,14 +1,14 @@
 export default {
     render(h) {
         return <footer class="footer">
-            <div class="footer_obj">
-                <span class="footer__copyright">© 2018 Gang of Dezigners</span>
-                <img class="footer__octopus"
+            <span class="footer__copyright">© 2018 Gang of Dezigners</span>
+            <div class="footer__github">
+                <img
                     src="/static/img/github-logo-black.png"
                     alt="Git:"
-                    height="30"
-                    width="30"/>
-                <a class="footer__github" href="https://github.com/BugLight/gang-of-dezigners">
+                    height="20"
+                    width="20"/>
+                <a href="https://github.com/BugLight/gang-of-dezigners">
                     Gang of Dezigners GitHub page
                 </a>
             </div>
