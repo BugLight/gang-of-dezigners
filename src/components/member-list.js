a955464da1a9b415ad539eff019f9b2933298d09
Buglight <buglight@kistriver.com>
2018-04-06 18:52:54 +0300

    Image cropping

--- src/components/member-list.js
+++ src/components/member-list.js
@@ -1,17 +1,50 @@
+import member from './member'
+
 export default {
+    components: {
+        'member': member
+    },
     data () {
         return {
             members: [
-                'first',
-                'second',
-                'third',
-                'forth'
+                {
+                    nick: 'buglight',
+                    first: 'Даниил',
+                    last: 'Жуков',
+                    group: '',
+                    desc: '',
+                    photo: '/static/maket.jpg'
+                },
+                {
+                    nick: 'talkytitan5127',
+                    first: 'Павел',
+                    last: '',
+                    group: '',
+                    desc: '',
+                    photo: '/static/maket.jpg'
+                },
+                {
+                    nick: 'pettro98',
+                    first: 'Петр',
+                    last: '',
+                    group: '',
+                    desc: '',
+                    photo: '/static/maket.jpg'
+                },
+                {
+                    nick: 'h1kk4',
+                    first: 'Алексей',
+                    last: '',
+                    group: '',
+                    desc: '',
+                    photo: '/static/maket.jpg'
+                }
             ]
         };
     },
     render (h) {
         return <ul class="member-list">
-            {this.members.map(m => <li>{m}</li>)}
+            {this.members.map(m => <li><member info={m}></member></li>)}
         </ul>;
     }
 };
