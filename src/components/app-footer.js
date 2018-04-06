fa8e8032a8d29bd45f53b26ebad283bea7b26733
Gorbunov Pavel <pachagorbunoff@yandex.ru>
2018-04-06 16:45:05 +0300

    footer pin to bottom and corrcet text place

--- src/components/app-footer.js
+++ src/components/app-footer.js
@@ -1,20 +1,23 @@
 export default {
   render(h) {
-    return <footer class="footer">
-        <span class="footer__copyright">© 2018 Gang of Designers</span>
-        <img
-        class="footer__octopus"
-          src="/static/github-logo-black.png"
-          alt="Git:"
-          height="30"
-          width="30"
-        />
-        <a
-          class="footer__github"
-          href="https://github.com/BugLight/gang-of-dezigners"
-        >
-          Gang of Dezigners GitHub page
-        </a>
+    return 
+      <footer class="footer">
+        <div class="footer_obj">
+          <span class="footer__copyright">© 2018 Gang of Designers</span>
+          <img
+          class="footer__octopus"
+            src="/static/github-logo-black.png"
+            alt="Git:"
+            height="30"
+            width="30"
+          />
+          <a
+            class="footer__github"
+            href="https://github.com/BugLight/gang-of-dezigners"
+          >
+            Gang of Dezigners GitHub page
+          </a>
+        </div>
       </footer>
   }
 };
