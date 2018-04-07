a53fde1a7be8a06450745fd0d518a5f87ba72b1e
pettro98 <ya.skywalker@yandex.ru>
2018-04-07 11:53:40 +0300

    added logo icon ant about text

--- src/components/app.js
+++ src/components/app.js
@@ -1,19 +1,30 @@
-import app_header from './app-header'
-import member_list from './member-list'
-import member from './member'
-import app_footer from './app-footer'
+import app_header from "./app-header";
+import member_list from "./member-list";
+import member from "./member";
+import app_footer from "./app-footer";
 
 export default {
-    components: {
-        'app-header': app_header,
-        'member-list': member_list,
-        'app-footer': app_footer
-    },
-    render (h) {
-        return <div>
-            <app-header></app-header>
-            <member-list></member-list>
-            <app-footer></app-footer>
-        </div>;
-    }
+  components: {
+    "app-header": app_header,
+    "member-list": member_list,
+    "app-footer": app_footer
+  },
+  render(h) {
+    return <div>
+        <app-header />
+        <member-list />
+        <br/>
+          <h2 class="about-us">О нас</h2>
+          <br />
+          <p class="about-us">
+            Мы - команда! Мы дружны, и сколько бы проблем и разногласий не
+            было между нами, любые дедлайны отступят, когда узреют, с каким
+            рвением мы стремимся к победе! Смех продлевает жизнь, а потому
+            анекдоты и мемы являюстся неотъемлемой частью нашей команды,
+            ведь в жизни самое важное чтобы было где повесить шляпу, которая
+            как раз.
+          </p>
+        <app-footer />
+      </div>;
+  }
 };
