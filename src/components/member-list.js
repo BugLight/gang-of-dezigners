c11f79edb58a426a884484483541ff0d1ac5c5dd
pettro98 <ya.skywalker@yandex.ru>
2018-04-08 19:07:07 +0300

    added description pettro98

--- src/components/member-list.js
+++ src/components/member-list.js
@@ -34,15 +34,10 @@ export default {
                     last: 'Толкунов',
                     second: 'Алексеевич',
                     group: 'ИУ8-41',
-                    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing \
-                    elit, sed do eiusmod tempor incididunt ut labore et dolore \
-                    magna aliqua. Ut enim ad minim veniam, quis nostrud \
-                    exercitation ullamco laboris nisi ut aliquip ex ea commodo \
-                    consequat. Duis aute irure dolor in reprehenderit in \
-                    voluptate velit esse cillum dolore eu fugiat nulla \
-                    pariatur. Excepteur sint occaecat cupidatat non proident, \
-                    sunt in culpa qui officia deserunt mollit anim id est \
-                    laborum.',
+                    desc: 'Волею судьбы занимаюсь дизайном, хотя предпочел бы \
+                    серверную часть. Иногда интуитивно вставляю костыли, \
+                    которые непостижимым для него образом оказываются \
+                    рабочими. Не люблю Python из-за не-си-подобного синтаксиса.',
                     photo: '/static/img/pettro98.jpg',
                     inactive: false
                 },
